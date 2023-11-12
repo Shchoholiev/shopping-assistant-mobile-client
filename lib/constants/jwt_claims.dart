dd742882fd312b1431d5710b6bb7927e1fc547e6
cuqmbr <me@cuqmbr.xyz>
2023-11-12 10:01:21 +0200

    add authentication service

---
+++ lib/constants/jwt_claims.dart
@@ -0,0 +1,10 @@
+class JwtClaims {
+  
+  static const String id = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier';
+
+  static const String email = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress';
+
+  static const String phone = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/mobilephone';
+
+  static const String roles ='http://schemas.microsoft.com/ws/2008/06/identity/claims/role';
+}
---
+++ lib/models/global_instances/global_user.dart
@@ -0,0 +1,10 @@
+class GlobalUser {
+
+  static String? id;
+
+  static String? email;
+
+  static String? phone;
+
+  static List<String>? roles;
+}
